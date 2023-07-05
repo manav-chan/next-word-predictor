d2b1407882d6d075592815fd1ba00c50034db88c
Manav Chauhan <manavchan03@gmail.com>
2023-07-05 08:17:41 +0530

    Added all files from local system to GitHub repository

---
+++ main.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:af291a74a1d1f119b4b8f5a329109e7d2254d79537a82f4c71f9d44d3969d630
+size 971
---
+++ static/script.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:aa32b3ae27261fba94859047cd5fa500d881b069ed3cc515dc97861624c2090b
+size 1477
